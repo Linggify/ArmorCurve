2c471e1b507465762d436dda7fd3ab2bdea62762
Linggify <frederik.lingg48@googlemail.com>
2022-05-01 13:40:44 +0200

    Fixed target for ValueUpdateMixin to match the earlier equivalent

--- src/main/java/jackiecrazy/armorcurve/mixin/ValueUpdateMixin.java
+++ src/main/java/jackiecrazy/armorcurve/mixin/ValueUpdateMixin.java
@@ -1,23 +1,23 @@
 package jackiecrazy.armorcurve.mixin;
 
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class ValueUpdateMixin {
 
     @Shadow public abstract AttributeInstance getAttribute(Attribute attribute);
 
-    @Inject(at = @At("HEAD"), method = "actuallyHurt")
-    protected void applyArmorToDamage(DamageSource source, float damage, CallbackInfo cbInfo) {
+    @Inject(at = @At("HEAD"), method = "getDamageAfterArmorAbsorb")
+    protected void applyArmorToDamage(DamageSource source, float damage, CallbackInfoReturnable<Float> cbInfo) {
         ((AttributeUpdater)(this.getAttribute(Attributes.ARMOR))).invokeUpdateAttribute();
     }
 }
